f9d4f2158fb186c38dcf25a0b4f46fb4b6cba4c5
kirillnemykin <k.nemykin16@gmail.com>
2023-07-11 23:05:00 +0300

    update task_2_2

--- homework/homework_2/Task_2_2.java
+++ homework/homework_2/Task_2_2.java
@@ -9,31 +9,32 @@ public class Task_2_2 {
 
         BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
 
-        System.out.println("Введіть кількість робочих годин на тиждень:");
-
+        System.out.println("Enter the number of working hours per week:");
         int hours = Integer.parseInt(reader.readLine());
-
-        System.out.println("Введіть вартість години:");
-
+        System.out.println("Enter the cost of an hour:");
         double cost = Double.parseDouble(reader.readLine());
-
-        System.out.println("Введіть відсоток податку:");
-
+        System.out.println("Enter the tax percentage:");
         double tax = Double.parseDouble(reader.readLine());
 
-        double monthSalaryWithTaxes = cost * hours * 4;
+        System.out.println("Salary per month with taxes: " + calculateMonthSalaryWithTaxes(hours, cost));
+        System.out.println("Salary per month without taxes: " + calculateMonthSalaryWithoutTaxes(hours, cost, tax));
+        System.out.println("Salary per year with taxes: " + calculateYearSalaryWithTaxes(hours, cost));
+        System.out.println("Salary per year without taxes: " + calculateYearSalaryWithoutTaxes(hours, cost, tax));
 
-        double monthSalaryWithoutTaxes = monthSalaryWithTaxes - (monthSalaryWithTaxes * tax/100);
-
-        double yearSalaryWithTaxes = monthSalaryWithTaxes * 12;
-
-        double yearSalaryWithoutTaxes = monthSalaryWithoutTaxes * 12;
-
-        System.out.println("Зарплатня на місяць без податків: " + monthSalaryWithoutTaxes);
-        System.out.println("Зарплатня на місяць з податками: " + monthSalaryWithTaxes);
-        System.out.println("Зарплатня на рік без податків: " + yearSalaryWithoutTaxes);
-        System.out.println("Зарплатня на рік з податками: " + yearSalaryWithTaxes);
-
-
+    }
+    public static double calculateMonthSalaryWithTaxes(int hours, double cost){
+        return cost * hours * 4;
+    }
+    public  static double calculateMonthSalaryWithoutTaxes(int hours, double cost, double tax){
+        double monthSalaryWithTaxes = calculateMonthSalaryWithTaxes(hours, cost);
+        return monthSalaryWithTaxes - (monthSalaryWithTaxes * tax/100);
+    }
+    public static double calculateYearSalaryWithTaxes(int hours, double cost){
+        double monthSalaryWithTaxes = calculateMonthSalaryWithTaxes(hours, cost);
+        return monthSalaryWithTaxes*12;
+    }
+    public static double calculateYearSalaryWithoutTaxes(int hours, double cost, double tax){
+        double monthSalaryWithoutTaxes = calculateMonthSalaryWithoutTaxes(hours, cost, tax);
+        return monthSalaryWithoutTaxes*12;
     }
 }
