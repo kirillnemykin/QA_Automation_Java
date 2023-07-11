3f646315aa6fb8f34c6bc456351cb2075e262ca7
kirillnemykin <k.nemykin16@gmail.com>
2023-07-12 00:50:04 +0300

    update task_4_4

--- homework/homework_4/Task_4_4.java
+++ homework/homework_4/Task_4_4.java
@@ -7,29 +7,28 @@ public class Task_4_4 {
     public static void main(String[] args) {
         double cost = Double.parseDouble(args[0]);
         double tax = Double.parseDouble(args[1]);
-        int[] workingDays = new int[12];
-        String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-        double salaryWithTaxes;
-        double salaryWithoutTaxes;
-        double totalWithTaxes = 0;
-        double totalWithoutTaxes = 0;
+
+        int[] workingDays = calculateWorkingDays();
+        printSalaryReport(cost, tax, workingDays);
+    }
+    public static int[] calculateWorkingDays(){
+        int daysOfMonth;
         int previousWeekDays = 5;
         int nextWeekDays = 0;
         int fullWeekDays;
-        int allDays;
         int numWorkingDays;
+        int[] workingDays = new int[12];
 
         for (int i = 0; i < 12; i++) {
             if (i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11){
-                allDays = 31;
+                daysOfMonth = 31;
             } else if (i == 3 || i == 5 || i == 8 || i == 10) {
-                allDays = 30;
+                daysOfMonth = 30;
             } else {
-                allDays = 28;
+                daysOfMonth = 28;
             }
-
-            fullWeekDays = (allDays-(7-nextWeekDays))/7*5;
-            nextWeekDays = (allDays-(7-nextWeekDays))%7;
+            fullWeekDays = (daysOfMonth-(7-nextWeekDays))/7*5;
+            nextWeekDays = (daysOfMonth-(7-nextWeekDays))%7;
             if (nextWeekDays == 6) {
                 numWorkingDays = previousWeekDays + fullWeekDays + nextWeekDays - 1;
             } else {
@@ -41,6 +40,14 @@ public class Task_4_4 {
                 previousWeekDays = 0;
             }
         }
+        return workingDays;
+    }
+    public static void printSalaryReport(double cost, double tax, int[] workingDays){
+        String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+        double salaryWithTaxes;
+        double salaryWithoutTaxes;
+        double totalWithTaxes = 0;
+        double totalWithoutTaxes = 0;
 
         for (int i = 0; i < workingDays.length; i++) {
             salaryWithTaxes = workingDays[i] * 8 * cost;
@@ -50,5 +57,5 @@ public class Task_4_4 {
             System.out.println(months[i] + " " + salaryWithoutTaxes + " " + salaryWithTaxes);
         }
         System.out.println("TOTAL: " + totalWithoutTaxes + " " + totalWithTaxes);
-}
+    }
 }
