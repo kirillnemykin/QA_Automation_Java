ec478178d38281e242582b1a16187ef6ec94ed51
kirillnemykin <k.nemykin16@gmail.com>
2023-07-12 00:14:02 +0300

    update task_3_3

--- homework/homework_3/Task_3_3.java
+++ homework/homework_3/Task_3_3.java
@@ -5,8 +5,21 @@ public class Task_3_3 {
         int month = Integer.parseInt(args[0]);
         double cost = Double.parseDouble(args[1]);
         double tax = Double.parseDouble(args[2]);
-        int days;
 
+        int days = calculateDays(month);
+        double salaryWithTaxes = calculateSalaryWithTaxes(days, cost);
+        double salaryWithoutTaxes = calculateSalaryWithoutTaxes(salaryWithTaxes, tax);
+
+        if (days == 0) {
+            System.out.println("You must enter a valid month number (1 to 12).");
+        } else {
+            System.out.println("Monthly salary with taxes is: " + salaryWithTaxes);
+            System.out.println("Monthly salary without taxes is: " + salaryWithoutTaxes);
+        }
+
+    }
+    public static int calculateDays(int month) {
+        int days;
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             days = 23;
         } else if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -16,16 +29,12 @@ public class Task_3_3 {
         } else {
             days = 0;
         }
-
-        double salaryWithTaxes = days * 8 * cost;
-        double salaryWithoutTaxes = salaryWithTaxes - (salaryWithTaxes * tax/100);
-
-        if (days == 0) {
-            System.out.println("Необхідно ввести коректний номер місяця (від 1 до 12).");
-        } else {
-            System.out.println("Зарплатня за місяць з податками складає: " + salaryWithTaxes);
-            System.out.println("Зарплатня за місяць без податків складає: " + salaryWithoutTaxes);
-        }
-
+        return days;
+    }
+    public static double calculateSalaryWithTaxes(int days, double cost) {
+        return days * 8 * cost;
+    }
+    public static double calculateSalaryWithoutTaxes(double salaryWithTaxes, double tax){
+        return salaryWithTaxes - (salaryWithTaxes * tax/100);
     }
 }
